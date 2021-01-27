d3a470f2ae824d5b577102867ce5b24b2dcdce3a
Anthony Castanza <acastanza@ucsd.edu>
2021-01-27 11:41:44 -0800

    Revert local test info

--- src/run_gp_affyst_efc.R
+++ src/run_gp_affyst_efc.R
@@ -93,7 +93,7 @@ check.output.format(opts$qc.plot.format)
 destdir <- "cel_files"
 tryCatch(
    {
-      files.to.process <- GP.setup.input.files("/Users/acastanza/Downloads/Mouse ST 2.0 array/input_files.txt", "/Users/acastanza/Downloads/Mouse ST 2.0 array/output")
+      files.to.process <- GP.setup.input.files(opts$input.file, destdir)
       GP.affyst.efc(files.to.process, normalize, background.correct, opts$qc.plot.format, 
                     opts$clm.file, annotate.rows, opts$output.file.base)
    },
